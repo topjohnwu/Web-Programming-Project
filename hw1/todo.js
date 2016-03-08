0018d567f832c2f6fc1dcb968a2e310a713f40da
topjohnwu <topjohnwu@gmail.com>
2016-03-09 03:32:12 +0800

    Add HW1

---
+++ hw1/todo.js
@@ -0,0 +1,52 @@
+var input = document.getElementById('input');
+var button = document.getElementById('button');
+var list = document.getElementById('list');
+var count = document.getElementById('count');
+
+var num = 0;
+
+input.onkeydown = newItem;
+button.onclick = removeDone;
+
+function newItem() {
+	if(event.keyCode != 13) return;
+	var item = document.createElement('li');
+	item.innerHTML = 
+	'<div><input type="checkbox"></input><label>' + input.value + '</label></div>';
+	input.value = '';
+	item.childNodes[0].childNodes[0].onclick = function () {
+		var label  = item.childNodes[0].childNodes[1];
+		if(event.target.checked === true) {
+			label.className = 'complete';
+			--num;
+		}
+		else {
+			label.className = '';
+			++num;
+		}
+		updateCount();
+	};
+	list.appendChild(item);
+	++ num;
+	updateCount();
+}
+
+function removeDone() {
+	var array = list.childNodes;
+	for(var i = 0; i < array.length; ++i) {
+		if(array[i].childNodes[0].childNodes[0].checked === true) {
+			list.removeChild(array[i]);
+			--i;
+		}
+	}
+}
+
+function updateCount() {
+	if (num === 0) {
+		count.innerHTML = 'No item left';
+	}
+	else if (num === 1) {
+		count.innerHTML = num + ' item left';
+	}
+	else count.innerHTML = num + ' items left';
+}
