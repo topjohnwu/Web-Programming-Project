409ca5769866dda3000eb52317026b6ee4ca2ecf
topjohnwu <topjohnwu@gmail.com>
2016-04-07 00:11:42 +0800

    HW5: Done

--- hw5/server.js
+++ hw5/server.js
@@ -5,6 +5,13 @@ var path = require('path');
 var app = express();
 var router = express.Router();
 
+function genError(status, message){
+    var err = new Error();
+    err.status = status;
+    err.message = message;
+    return err;
+}
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'nunjucks');
 nunjucks.configure('views', {
@@ -15,18 +22,15 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
-    next();
+    res.render('index');
 });
 
 router.get('/api/query', function(req, res, next){
     res.json(req.query);
-    next();
 });
 
 router.post('/api/body', function(req, res, next) {
   res.json(req.body);
-  next();
 });
 
 router.get('/api/user/:id', function(req, res, next){
@@ -38,10 +42,22 @@ router.get('/api/user/:id', function(req, res, next){
         var john = {id: 2, name: "John", age: 22};
         res.json(john);
     }
+    else next(genError(404, "ID: " + req.params.id + " not found!"));
 });
 
 router.use('/static', express.static('public'));
 
+router.use(function(req, res, next){
+    next(genError(404, "Page not found!!"));
+});
+
 app.use('/', router);
+
+app.use(function(err, req, res, next){
+    res.status(err.status).render('error', {
+        error: err.status,
+        message: err.message
+    });
+});
 
 app.listen(3000);
