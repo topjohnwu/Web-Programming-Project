2fea0cc1edb2a8a21e865a719e45dbc69a72f4c7
topjohnwu <topjohnwu@gmail.com>
2016-04-06 22:53:14 +0800

    Add HW5: GET <h1>首頁</h1>

---
+++ hw5/server.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var nunjucks = require('nunjucks');
+var path = require('path');
+var app = express();
+var router = express.Router();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'nunjucks');
+nunjucks.configure('views', {
+    autoescape: true,
+    express: app
+});
+
+router.get('/', function(req, res, next) {
+    res.render('index', { title: 'Express' });
+    next();
+});
+
+
+app.use('/', router);
+
+app.listen(3000);
