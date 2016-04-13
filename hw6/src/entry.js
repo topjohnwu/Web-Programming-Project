d6c94845f8518a6d1fa9b99a128326d55cf6f0ad
topjohnwu <topjohnwu@gmail.com>
2016-04-13 22:17:16 +0800

    HW6: Done

---
+++ hw6/src/entry.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import {render} from 'react-dom';
+import ChatApp from './component/ChatApp'
+import './style.css'
+
+render(<ChatApp/>, document.getElementById('root'));
